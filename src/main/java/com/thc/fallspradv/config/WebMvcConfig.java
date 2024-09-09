5d95785b3b241cbb4e02316889452b3695138a87
7SH7 <kimseunghwan7777@gmail.com>
2024-09-10 00:40:23 +0900

    feat: interceptor 구현 공부(spring security가 가장 밖에, controller 진입 전에 있는 게 interceptor

--- src/main/java/com/thc/fallspradv/config/WebMvcConfig.java
+++ src/main/java/com/thc/fallspradv/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.thc.fallspradv.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
+import com.thc.fallspradv.interceptor.DefaultInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -15,12 +17,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // 절대경로 file:///c:/upload/
         // 상대경로 file:./upload/
         registry
-                .addResourceHandler("/files/**")
-                //접근할 파일이 있는 위치를 지정
-                .addResourceLocations("file:" + "C:/workspace/uploadfiles/")
-                .setCachePeriod(60 * 60) // 초 단위
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver());
+            .addResourceHandler("/files/**")
+            //접근할 파일이 있는 위치를 지정
+            .addResourceLocations("file:" + "C:/workspace/uploadfiles/")
+            .setCachePeriod(60 * 60) // 초 단위
+            .resourceChain(true)
+            .addResolver(new PathResourceResolver());
+    }
+
+    // spring security 다음, interceptor가 있고, 이후, controller가 있다.
+    // interceptor 설정
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+            .addInterceptor(new DefaultInterceptor())
+            .addPathPatterns("/api/**") // interceptor가 실행되어야 하는 url 패턴 설정  -> 여기 속한 api만 인터셉터가 실행됨
+            .excludePathPatterns(
+                "/resources/**", "/api/tbuser/logout"); // interceptor가 실행되지 않아야 하는 url 패턴 설정
     }
 
 }
--- src/main/java/com/thc/fallspradv/controller/TbpostRestController.java
+++ src/main/java/com/thc/fallspradv/controller/TbpostRestController.java
@@ -6,7 +6,10 @@ import com.thc.fallspradv.service.TbpostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +18,12 @@ import java.util.List;
 
 
 @Tag(name = "1-1. 게시글 API 안내",
-        description = "게시글 관련 기능 정의한 RestController.")
+    description = "게시글 관련 기능 정의한 RestController.")
 @RequestMapping("/api/tbpost")
 @RestController
 public class TbpostRestController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final TbpostService tbpostService;
     public TbpostRestController(TbpostService tbpostService) {
@@ -28,10 +33,10 @@ public class TbpostRestController {
     /**/
 
     @Operation(summary = "게시글 생성",
-            description = "게시글 생성 컨트롤러 <br />"
-                    + "@param TbpostDto.CreateReqDto <br />"
-                    + "@return HttpStatus.CREATED(201) ResponseEntity\\<TbpostDto.CreateResDto\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
+        description = "게시글 생성 컨트롤러 <br />"
+            + "@param TbpostDto.CreateReqDto <br />"
+            + "@return HttpStatus.CREATED(201) ResponseEntity\\<TbpostDto.CreateResDto\\> <br />"
+            + "@exception 필수 파라미터 누락하였을 때 등 <br />"
     )
     @PostMapping("")
     public ResponseEntity<TbpostDto.CreateResDto> create(@Valid @RequestBody TbpostDto.CreateReqDto param){
@@ -39,10 +44,10 @@ public class TbpostRestController {
     }
 
     @Operation(summary = "게시글 수정",
-            description = "게시글 수정 컨트롤러 <br />"
-                    + "@param TbpostDto.UpdateReqDto <br />"
-                    + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.CreateResDto\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
+        description = "게시글 수정 컨트롤러 <br />"
+            + "@param TbpostDto.UpdateReqDto <br />"
+            + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.CreateResDto\\> <br />"
+            + "@exception 필수 파라미터 누락하였을 때 등 <br />"
     )
     @PutMapping("")
     public ResponseEntity<TbpostDto.CreateResDto> update(@Valid @RequestBody TbpostDto.UpdateReqDto param){
@@ -50,20 +55,20 @@ public class TbpostRestController {
     }
 
     @Operation(summary = "게시글 상세 조회",
-            description = "게시글 상세 조회 컨트롤러 <br />"
-                    + "@param TbpostDto.DetailReqDto <br />"
-                    + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.DetailResDto\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
+        description = "게시글 상세 조회 컨트롤러 <br />"
+            + "@param TbpostDto.DetailReqDto <br />"
+            + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.DetailResDto\\> <br />"
+            + "@exception 필수 파라미터 누락하였을 때 등 <br />"
     )
     @GetMapping("")
     public ResponseEntity<TbpostDto.DetailResDto> detail(@Valid DefaultDto.DetailReqDto param, HttpServletRequest request){
         return ResponseEntity.status(HttpStatus.OK).body(tbpostService.detail(param));
     }
     @Operation(summary = "게시글 목록 전체 조회",
-            description = "게시글 목록 전체 조회 컨트롤러 <br />"
-                    + "@param TbpostDto.ListReqDto <br />"
-                    + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.DetailResDto\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
+        description = "게시글 목록 전체 조회 컨트롤러 <br />"
+            + "@param TbpostDto.ListReqDto <br />"
+            + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.DetailResDto\\> <br />"
+            + "@exception 필수 파라미터 누락하였을 때 등 <br />"
     )
     @GetMapping("/list")
     public ResponseEntity<List<TbpostDto.DetailResDto>> list(@Valid TbpostDto.ListReqDto param){
@@ -71,23 +76,27 @@ public class TbpostRestController {
     }
 
     @Operation(summary = "게시글 목록 페이지 조회",
-            description = "게시글 목록 페이지 조회 컨트롤러 <br />"
-                    + "@param TbpostDto.PagedListReqDto <br />"
-                    + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.PagedListResDto\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
+        description = "게시글 목록 페이지 조회 컨트롤러 <br />"
+            + "@param TbpostDto.PagedListReqDto <br />"
+            + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.PagedListResDto\\> <br />"
+            + "@exception 필수 파라미터 누락하였을 때 등 <br />"
     )
     @GetMapping("/plist")
     public ResponseEntity<DefaultDto.PagedListResDto> plist(@Valid TbpostDto.PagedListReqDto param){
         return ResponseEntity.status(HttpStatus.OK).body(tbpostService.pagedList(param));
     }
     @Operation(summary = "게시글 목록 스크롤 조회",
-            description = "게시글 목록 스크롤 조회 컨트롤러 <br />"
-                    + "@param TbpostDto.MoreListReqDto <br />"
-                    + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.DetailResDto\\> <br />"
-                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
+        description = "게시글 목록 스크롤 조회 컨트롤러 <br />"
+            + "@param TbpostDto.MoreListReqDto <br />"
+            + "@return HttpStatus.OK(200) ResponseEntity\\<TbpostDto.DetailResDto\\> <br />"
+            + "@exception 필수 파라미터 누락하였을 때 등 <br />"
     )
     @GetMapping("/mlist")
-    public ResponseEntity<List<TbpostDto.DetailResDto>> mlist(@Valid TbpostDto.ScrollListReqDto param){
+    public ResponseEntity<List<TbpostDto.DetailResDto>> mlist(@Valid TbpostDto.ScrollListReqDto param
+        , HttpServletRequest request, HttpServletResponse response){
+        logger.info("reqTest : " + request.getAttribute("reqTest"));
+        logger.info("resTest : " + response.getHeader("resTest"));
+
         return ResponseEntity.status(HttpStatus.OK).body(tbpostService.scrollList(param));
     }
 
